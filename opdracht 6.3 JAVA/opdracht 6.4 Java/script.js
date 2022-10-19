b1a2ea8a8764b6a0c8c22682b371867105155ef1
Yassine6262 <yassineaz2018@outlook.com>
2022-10-19 14:13:01 +0200

    6.2, 6.3, 6.4

---
+++ opdracht 6.3 JAVA/opdracht 6.4 Java/script.js
@@ -0,0 +1,27 @@
+function kleur_aanpassen() {
+    document.getElementById("h1").style.color ="red";
+    document.getElementById("h1").innerText = "tekst veranderd";
+}
+
+function calculater(x, y) {
+    let answer = x + y;
+    console.log(answer);
+    }
+    calculater (6, 7);
+    
+    
+    
+    function multiplybyfive(x) {
+        let answer = x * 5;
+        console.log(answer);
+        }
+        
+    
+    
+        function calculatermintutes(num){
+            let answer = num /60;
+            console.log(num / 60)
+    
+        }
+    
+        calculatermintutes(3600)
